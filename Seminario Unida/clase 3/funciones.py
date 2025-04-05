0e87fec630bf216160538eacdc7a0b69d73367c4
Migsos <sosaalfonso@hotmail.com>
2025-04-05 13:17:42 -0300

    tercera clase

---
+++ Seminario Unida/clase 3/funciones.py
@@ -0,0 +1,30 @@
+'''
+Ejemplos de funciones
+
+'''
+
+def sumar(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+def multp(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def resto(a,b):
+    return a%b
+
+
+n1=3
+n2=5
+resultado=sumar(n1,n2)
+print(f"La suma es: ",{resultado})
+resultado=resta(n1,n2)
+print(f"La resta es: ",{resultado})
+resultado=multp(n1,n2)
+print(f"La multp es: ",{resultado})
+resultado=div(n1,n2)
+print(f"La div es: ",{resultado})
+resultado=resto(n1,n2)
+print(f"La resto es: ",{resultado})
---
+++ Seminario Unida/clase 3/while.py
@@ -0,0 +1,9 @@
+'''
+While - mientras que
+
+'''
+
+i=1;
+while i<= 20:
+    print("Unida","indice",i)
+    i+=1
